40530a017c3387ca57bcfbf172b04e70f7318cd8
Antonio K <antoniokrivic@gmail.com>
2022-10-05 15:43:31 +0200

    Summary

---
+++ DataAdapterInAdoDotNet/DataAdapterInAdoDotNet/DataAdapterForm.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataAdapterForm.aspx.cs" Inherits="DataAdapterInAdoDotNet.DataAdapterForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:GridView ID="GridView2" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnDataTable" runat="server" OnClick="btnDataTable_Click" Text="DataTable" />
+            <asp:Button ID="btnDataSet" runat="server" OnClick="btnDataSet_Click" Text="DataSet" Width="85px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataAdapterInAdoDotNet/DataAdapterInAdoDotNet/DataAdapterForm.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAdapterInAdoDotNet
+{
+    public partial class DataAdapterForm : System.Web.UI.Page
+    {
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void btnDataTable_Click(object sender, EventArgs e)
+        {
+            PutGridToNull();
+            string connection = "Data Source=ST-02\\SQLEXPRESS; Initial Catalog=master; Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connection);
+            string query = "SELECT * FROM Orders";
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            Label1.Text = "DataTable";
+        }
+
+        protected void btnDataSet_Click(object sender, EventArgs e)
+        {
+            PutGridToNull();
+            string connection = "Data Source=ST-02\\SQLEXPRESS; Initial Catalog=master; Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connection);
+            
+
+            string query1 = "SELECT * FROM Orders";
+            SqlDataAdapter da = new SqlDataAdapter(query1, con);
+            da.Fill(ds,"tableOrders");
+
+            string query2 = "SELECT * FROM People";
+            da = new SqlDataAdapter(query2, con);
+            da.Fill(ds, "tablePeople");
+
+            BindedTablesForDataSet();
+
+            Label1.Text = "DataSet";
+        }
+
+        protected void PutGridToNull()
+        {
+            GridView1.DataSource = null;
+            GridView1.DataBind();
+            GridView2.DataSource = null;
+            GridView2.DataBind();
+        }
+
+        protected void BindedTablesForDataSet()
+        {
+            GridView1.DataSource = ds.Tables["tableOrders"];
+            GridView1.DataBind();
+            GridView2.DataSource = ds.Tables["tablePeople"];
+            GridView2.DataBind();
+        }
+    }
+}
